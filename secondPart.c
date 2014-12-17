9ac12cdec4e681f64923cfca384702b6b475430f
Zhanlin Wang <zhanlin_wang@126.com>
2014-12-17 18:50:38 +0000

    Change the second part integration to Dawson function and error function,the performance is improved by 10%.

--- secondPart.c
+++ secondPart.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <complex.h>
 #include <math.h>
+#include <complex.h>
 
-#include <gsl/gsl_integration.h>
+//#include <gsl/gsl_integration.h>
 #include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_dawson.h>
 
 #include "zetaFunc.h"
 /***************************************************
@@ -18,13 +19,30 @@ double complex secondPart(const int l, const double gamma, const double Lamda, c
 {
   int s1 = 0, s2 = 0;
   double complex secondPartInt = 0+0*I;
+	//double inteCore = 0.0;
   if(l != 0){
     secondPartInt = 0.0;
   }
   else{
+		//Integration method.
     secondPartInt = spheHarm(0, 0, 0, 0, &s1) * gamma * pow(M_PI,3.0/2.0) 
       * ( 2 * qSqur * sndInteFunc(Lamda, qSqur, &s2)
           -2 * exp(Lamda * qSqur)/sqrt(Lamda));
+		/*
+		//Arithmetic method.
+		  if(qSqur > 0){
+				inteCore = 4 * sqrt(qSqur) * exp(Lamda * qSqur) * gsl_sf_dawson(sqrt(Lamda * qSqur)); 
+			}else 
+				if(qSqur == 0){
+					inteCore = 0;
+				}else
+					if(qSqur < 0){
+						inteCore = -2 * sqrt(M_PI) * sqrt( - qSqur) * erf( sqrt( - Lamda * qSqur) );
+					}
+
+			secondPartInt = spheHarm(0, 0, 0, 0, &s1) * gamma * pow(M_PI,3.0/2.0)
+				* ( inteCore 	-  2 * exp(Lamda * qSqur)/sqrt(Lamda));
+		*/
   }
   *rstatus = s1 + s2;
   //printf("Lamda=%lf,qSqur = %.4f\nsecondPartInt = %.24f \n", Lamda, qSqur, secondPartInt);
