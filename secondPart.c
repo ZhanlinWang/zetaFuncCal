57672731a05ac6e85f10fc45afb6ed5eae0494c7
Zhanlin Wang <zhanlin_wang@126.com>
2014-12-18 10:45:12 +0000

    Solve a little variable definition problem

--- secondPart.c
+++ secondPart.c
@@ -19,7 +19,6 @@ double complex secondPart(const int l, const double gamma, const double Lamda, c
 {
   int s1 = 0, s2 = 0;
   double complex secondPartInt = 0+0*I;
-	//double inteCore = 0.0;
   if(l != 0){
     secondPartInt = 0.0;
   }
@@ -31,6 +30,7 @@ double complex secondPart(const int l, const double gamma, const double Lamda, c
           -2 * exp(Lamda * qSqur)/sqrt(Lamda));
 		*/
 		//Arithmetic method.
+			double inteCore = 0.0;
 		  if(qSqur > 0){
 				//Dawson function for qSqur>0
 				inteCore = 4 * sqrt(qSqur) * exp(Lamda * qSqur) * gsl_sf_dawson(sqrt(Lamda * qSqur)); 
