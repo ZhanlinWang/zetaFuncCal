e91fcbb288f07ef61791e50c024e8c28aea0116c
Carsten Urbach <curbach@gmx.de>
2015-01-16 12:22:53 +0100

    set rstatus to zero prior to calls of luscherZeta

--- zeta_function_R_interface.c
+++ zeta_function_R_interface.c
@@ -30,7 +30,6 @@ SEXP LuscherZetaArray(SEXP qsq_, SEXP n_, SEXP l_, SEXP m_, SEXP dvec_, SEXP gam
   const double tol = NUMERIC_POINTER(tol_)[0];
   double * dvec = NUMERIC_POINTER(dvec_);
   const int verbose = INTEGER_POINTER(verbose_)[0];
-  int rstatus[3];
   SEXP res;
   Rcomplex * resp;
   double ires[2];
@@ -38,20 +37,27 @@ SEXP LuscherZetaArray(SEXP qsq_, SEXP n_, SEXP l_, SEXP m_, SEXP dvec_, SEXP gam
   resp = COMPLEX_POINTER(res);
 
   for(int i = 0; i < n; i ++) {
+    int rstatus[3] = {0,0,0};
+
     luscherZeta(ires, qsq[i], l, m, gamma, lambda, dvec, tol, verbose, rstatus);
+
     resp[i].r = ires[0];
     resp[i].i = ires[1];
+
     if(rstatus[0]!=0) {
-      error("GSL error in evaluation of first contribution to Luescher Zeta function, status code %d\n", rstatus[0]);
-      break;
+      warning("GSL error in evaluation of first contribution to Luescher Zeta function, status code %d for element %d\n", rstatus[0], i+1);
+      resp[i].r = NA_REAL;
+      resp[i].i = NA_REAL;
     }
-    if(rstatus[1]!=0) {
-      error("GSL error in evaluation of second contribution to Luescher Zeta function, status code %d\n", rstatus[1]);
-      break;
+    else if(rstatus[1]!=0) {
+      warning("GSL error in evaluation of second contribution to Luescher Zeta function, status code %d for element %d\n", rstatus[1], i+1);
+      resp[i].r = NA_REAL;
+      resp[i].i = NA_REAL;
     }
-    if(rstatus[2]!=0) {
-      error("GSL error in evaluation of third contribution to Luescher Zeta function, status code %d\n", rstatus[2]);
-      break;
+    else if(rstatus[2]!=0) {
+      warning("GSL error in evaluation of third contribution to Luescher Zeta function, status code %d for element %d\n", rstatus[2], i+1);
+      resp[i].r = NA_REAL;
+      resp[i].i = NA_REAL;
     }
   }
 
